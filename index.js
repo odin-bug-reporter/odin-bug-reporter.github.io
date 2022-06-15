74d0cf6bab117131d1d8ca2bf9efe3a884eee2ab
odin-bug-reporter <107526143+odin-bug-reporter@users.noreply.github.com>
2022-06-15 02:16:44 -0400

    Set up first 2 cascading dropdown areas

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
 var reData = [
-  ["Resource1", "Error1"],
-  ["Resource1", "Error2"],
-  ["Resource2", "Error3"],
-  ["Resource2", "Error4"],
+  ["Resource1", "Error1 for Resource1"],
+  ["Resource1", "Error2 for Resource1"],
+  ["Resource1", "Error3 for Resource1"],
+  ["Resource2", "Error1 for Resource2"],
+  ["Resource2", "Error2 for Resource2"],
 ];
 
 function makeDropDown(data, resourceFilter) {
   const filteredArray = data.filter((r) => r[0] === resourceFilter);
-
-  const uniqueOptions = new Set();
-
-  filteredArray.forEach((r) => uniqueOptions.add(r[1]));
-
-  const uniqueList = [...uniqueOptions];
-
+  const uniqueList = getUniqueValues(filteredArray, 1);
   const selectError = document.getElementById("error");
-
-  uniqueList.forEach((item) => {
-    const option = document.createElement("option");
-    option.textContent = item;
-    selectError.appendChild(option);
-  });
+  popDropDown(selectError, uniqueList);
 }
 
 function applyDropDown() {
@@ -28,4 +18,31 @@ function applyDropDown() {
   makeDropDown(reData, selectResource);
 }
 
+function afterDocumentLoads() {
+  popResourceDropDown();
+  applyDropDown();
+}
+
+function getUniqueValues(data, index) {
+  const uniqueOptions = new Set();
+  data.forEach((r) => uniqueOptions.add(r[index]));
+  return [...uniqueOptions];
+}
+
+function popResourceDropDown() {
+  const uniqueList = getUniqueValues(reData, 0);
+  const el = document.getElementById("resource");
+  popDropDown(el, uniqueList);
+}
+
+function popDropDown(el, listAsArray) {
+  el.innerHTML = "";
+  listAsArray.forEach((item) => {
+    const option = document.createElement("option");
+    option.textContent = item;
+    el.appendChild(option);
+  });
+}
+
 document.getElementById("resource").addEventListener("change", applyDropDown);
+document.addEventListener("DOMContentLoaded", afterDocumentLoads);
