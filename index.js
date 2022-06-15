54f3b9e7ca81645c167eca18adaf936ebe2b5a74
odin-bug-reporter <107526143+odin-bug-reporter@users.noreply.github.com>
2022-06-14 23:58:31 -0400

    Added main form and dropdown button functionality

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-function display() {
-  alert("Hello World!");
-}
+var resourceObject = {
+  Resource1: {
+    Error1,
+    Error2,
+  },
+  Resource2: {
+    Error3,
+    Error4,
+  },
+};
+window.onload = function () {
+  var resourceSel = document.getElementById("resource");
+  var errorSel = document.getElementById("error");
+  for (var x in resourceObject) {
+    resourceSel.options[resourceSel.options.length] = new Option(x, x);
+  }
+  resourceSel.onchange = function () {
+    //empty errors- dropdowns
+    errorSel.length = 1;
+    //display correct values
+    for (var y in resourceObject[this.value]) {
+      errorSel.options[errorSel.options.length] = new Option(y, y);
+    }
+  };
+};
