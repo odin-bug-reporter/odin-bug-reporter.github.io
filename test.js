c394b0abeaab60af13b29d00e455a0f8c456e3fb
odin-bug-reporter <107526143+odin-bug-reporter@users.noreply.github.com>
2022-06-15 16:53:24 -0400

    Changes to json

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 var request = new XMLHttpRequest();
 
-request.open("GET", "http://127.0.0.1:5000/courses", true);
+request.open(
+  "GET",
+  "https://odin-bug-reporter.github.io/jsonapi/reData.json",
+  true
+);
 
 request.onload = function () {
   var reData = JSON.parse(this.response);
