9b25aeee0824bcd41bb6be9307b34ccda17411a4
odin-bug-reporter <107526143+odin-bug-reporter@users.noreply.github.com>
2022-06-15 01:15:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-var data = [
+var reData = [
   ["Resource1", "Error1"],
   ["Resource1", "Error2"],
   ["Resource2", "Error3"],
   ["Resource2", "Error4"],
 ];
 
-const filteredArray = data.filter((r) => r[0] === "Resource1");
+function makeDropDown(data, resourceFilter) {
+  const filteredArray = data.filter((r) => r[0] === resourceFilter);
 
-console.log(filteredArray);
+  const uniqueOptions = newSet();
+
+  filteredArray.forEach((r) => uniqueOptions.add(r[1]));
+
+  const uniqueList = [...uniqueOptions];
+
+  const selectError = document.getElementById("error");
+
+  uniqueList.forEach((item) => {
+    const option = document.createElement("option");
+    option.textContent = item;
+    selectError.appendChild(option);
+  });
+}
+
+function applyDropDown() {
+  const selectResource = document.getElementById("resource").value;
+  makeDropDown(reData, selectResource);
+}
+
+document.getElementById("resource").addEventListener("change", applyDropDown);
